40ad14cb6f0fdc6945858546e65bdc202eb3dfd3
Wayne Luke <wayneluke@me.com>
2021-09-13 16:04:10 -0700

    Update options.php
    - A little re-arrangement of contents.
    - Added some comments.
    - Fixed output directories.

--- options.php
+++ options.php
@@ -1,19 +1,27 @@
 <?php
+// This script writes out markdown files for each setting group. 
+
+
+// Only allow this to be run from the CLI.
+if (PHP_SAPI != 'cli') { die ('Not Allowed');} 
+
+// Get some important files.
 require_once('./includes/system.php');
 require_once('./includes/database.php');
 require_once('./includes/template.php');
 require_once('./includes/functions.php');
 
-if (PHP_SAPI != 'cli') 
-{ 
-   die ('Not Allowed');
-} 
 
-// Misc. Settings. These probably do not need to be changed.
-//$outDir='../pages/20.vbulletin_5.3.0/06.admin_control_panel/02.settings/01.options';
+// Setup System
+$sys = new System("./config/settings.ini", __DIR__);
+$dbConnect = new Database("./config/settings.ini");
+if (!empty($dbConnect)) {
+    echo "Database Connection Successful\n\r";
+}
 
+// Setup Variables for page generation.
 $separator=DIRECTORY_SEPARATOR;
-$templateTokens=['~title~','~title_slug~','~date~','~group~','~version~','~content~'];
+$templateTokens=['~title~','~title_slug~','~date~','~group~','~version~','~content~','~weight~'];
 $contentTokens=['~title~','~image~','~description~','~help~','~additionalinfo~','~varname~','~type~','~defaultvalue~'];
 $imageTokens=['~imageurl~','~caption~'];
 
@@ -30,13 +38,7 @@ $query =[
         WHERE s.grouptitle=? ORDER BY s.displayorder",
 ];
 
-$sys = new System("./config/settings.ini", __DIR__);
 
-$dbConnect = new Database("./config/settings.ini");
-
-if (!empty($dbConnect)) {
-    echo "Database Connection Successful\n\r";
-}
 
 $clean = true;
 $version = $dbConnect->run_query($query['version']);
@@ -61,12 +63,12 @@ foreach ($groups as $group) {
         $currentItem = new Template('setting');
         $content.=$currentItem->parse($contentTokens,$itemReplace);
     }
-    $groupDir = $outDir . $separator . $group['displayorder'] . '.' . $group['grouptitle'];
+    $groupDir = $outDir . $separator . $group['grouptitle'];
     createDirectory($groupDir);
-    $templateReplace=[$group['title'], slugify($group['title']), $now, $group['grouptitle'], $curVersion, $content];
+    $templateReplace=[$group['title'], slugify($group['title']), $now, $group['grouptitle'], $curVersion, $content, $group['displayorder']];
 
     $settingPage = new Template('setting.page');
     $page=$settingPage->parse($templateTokens,$templateReplace);
-    file_put_contents($groupDir . $separator . 'article.md', $page);
+    file_put_contents($groupDir . $separator . 'index.md', $page);
 }
 
